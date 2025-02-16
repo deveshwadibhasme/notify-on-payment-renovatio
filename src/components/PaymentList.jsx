263d9a683d1cd47fc9a5ccf4c5ec38afa01a1e5c
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-02-16 22:09:50 +0530

    add/login

--- src/components/PaymentList.jsx
+++ src/components/PaymentList.jsx
@@ -18,29 +18,33 @@ const PaymentList = ({handleDisplay}) => {
         <div className="container mx-auto mt-20 p-4 max-w-5xl border-1">
             <h1 className="text-2xl font-bold mb-4 text-center">Payment List</h1>
             <hr />
-            <table className="min-w-full bg-white">
-                <thead>
-                    <tr>
-                        <th className="py-2 px-4 border-b">Name</th>
-                        <th className="py-2 px-4 border-b">Mobile Number</th>
-                        <th className="py-2 px-4 border-b">Donation Amount</th>
-                        <th className="py-2 px-4 border-b">Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {(payments.length !== 0) ? payments[0].data.map((payment, index) => (
-                        <tr key={index}>
-                            <td className="py-2 px-4 border-b text-center">{payment.name}</td>
-                            <td className="py-2 px-4 border-b text-center">{payment.mobile}</td>
-                            <td className="py-2 px-4 border-b text-center">{payment.amount}</td>
-                            <td className="py-2 px-4 border-b text-center">{payment.email}</td>
+            <div className="overflow-x-auto overflow-y-auto">
+                <table className="min-w-full bg-white">
+                    <thead>
+                        <tr>
+                            <th className="py-2 px-4 border-b">Name</th>
+                            <th className="py-2 px-4 border-b">Mobile Number</th>
+                            <th className="py-2 px-4 border-b">Donation Amount</th>
+                            <th className="py-2 px-4 border-b">Email</th>
                         </tr>
-                    )):<tr className='text-center'><td>Noting To See Here</td></tr>}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {(payments.length !== 0) ? payments[0].data.map((payment, index) => (
+                            <tr key={index}>
+                                <td className="py-2 px-4 border-b text-center">{payment.name}</td>
+                                <td className="py-2 px-4 border-b text-center">{payment.mobile}</td>
+                                <td className="py-2 px-4 border-b text-center">{payment.amount}</td>
+                                <td className="py-2 px-4 border-b text-center">{payment.email}</td>
+                            </tr>
+                        )) : <tr className='text-center'><td colSpan="4">Nothing To See Here</td></tr>}
+                    </tbody>
+                </table>
+            </div>
             <div 
-        onClick={handleDisplay}
-        className="mx-auto bg-green-400 w-28 font-bold text-center mt-3 rounded-full p-1 cursor-pointer">Display Form</div>
+                onClick={handleDisplay}
+                className="mx-auto bg-green-400 w-28 font-bold text-center mt-3 rounded-full p-1 cursor-pointer">
+                Display Form
+            </div>
         </div>
     );
 };
